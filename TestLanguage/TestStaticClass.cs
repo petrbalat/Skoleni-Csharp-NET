ab7dbe7df03132763dcf5e021894e57062a5ae04
Petr Balat <balat@WBALATP.oksystem.local>
2012-05-30 13:04:47 +0200

    +static class

---
+++ TestLanguage/TestStaticClass.cs
@@ -0,0 +1,15 @@
+﻿namespace TestLanguage
+{
+    public static class TestStaticClass
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
