2cdc1353962581c013f983a9c6b52ff1a1b31056
Petr Balat <balat@WBALATP.oksystem.local>
2012-05-31 12:55:45 +0200

    + test generiky

---
+++ TestLanguage/TestGeneric.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TestLanguage
+{
+    [TestFixture]
+    public class TestGeneric
+    {
+        [Test]
+        public void Koovariance()
+        {
+            IEnumerable<ITestTrida> tridy = new List<ITestTrida> { new TestTrida("a"), new TestTrida("b") { Size = 2 } };
+            Console.WriteLine(tridy);
+        }
+
+        [Test]
+        public void KontraVariance()
+        {
+            SortedSet<TestTrida> testTridas = new SortedSet<TestTrida>(new TestTridaComparer())
+                                                  {
+                                                      new TestTrida("d") { Size = 5 },
+                                                      new TestTrida("e") { Size = 2 },
+                                                      new TestTrida("f") { Size = 8 },
+                                                  };
+            Console.WriteLine(testTridas);
+        }
+
+        [Test]
+        public void TestToString()
+        {
+            Console.WriteLine(typeof(IList<>));
+            Console.WriteLine(typeof(IList<string>));
+            Console.WriteLine(typeof(IDictionary<,>));
+            Console.WriteLine(typeof(IDictionary<int, string>));
+        }
+
+        public class TestTridaComparer : IComparer<ITestTrida>
+        {
+            public int Compare(ITestTrida x, ITestTrida y)
+            {
+                return x.Size.CompareTo(y.Size);
+            }
+        }
+    }
+}
--- TestLanguage/TestOperator.cs
+++ TestLanguage/TestOperator.cs
@@ -18,7 +18,14 @@ namespace TestLanguage
 
     public class Tvor
     {
+
+        #region PROPERTIES
+
         public Pohlavi Typ { get; set; }
+
+        #endregion PROPERTIES
+
+        #region OPERATORS
 
         public static Tvor operator +(Tvor davka1, Tvor davka2)
         {
@@ -30,6 +37,38 @@ namespace TestLanguage
             int pohlavi = new Random().Next(1, 2);
             return new Tvor { Typ = (Pohlavi)pohlavi };
         }
+
+        public static bool operator ==(Tvor davka1, Tvor davka2)
+        {
+            return Equals(davka1.Typ, davka2.Typ);
+        }
+
+        public static bool operator !=(Tvor davka1, Tvor davka2)
+        {
+            return !(davka1 == davka2);
+        }
+
+        public bool Equals(Tvor other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.Typ, Typ);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (Tvor)) return false;
+            return Equals((Tvor) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Typ.GetHashCode();
+        }
+
+        #endregion OPERATORS
     }
 
     public enum Pohlavi : int
