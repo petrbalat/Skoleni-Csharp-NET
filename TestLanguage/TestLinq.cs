f185f530a50398c58cce11bdde498e073cda4c83
Petr Balat <balat@WBALATP.oksystem.local>
2012-05-31 11:05:43 +0200

    doplneni C# casti prezentace

---
+++ TestLanguage/TestLinq.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TestLanguage
+{
+    [TestFixture]
+    public class TestLinq
+    {
+        [Test]
+        public void TestToObjects()
+        {
+            IEnumerable<string> cisla = from i in Enumerable.Range(1, 1000)
+                        let pom = i * 2
+                        where pom % 3 == 0
+                        orderby i descending 
+                        select i.ToString();
+
+
+            foreach (var i in cisla)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
